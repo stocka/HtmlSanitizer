26e5024092ab73fc47b9ef3e24dac9f86b2df1bf
Andrew Stock <totally@funoninter.net>
2014-01-15 00:45:25 -0500

    refactoring work and expansion
    HtmlSanitizer now behaves as a static class.
    Additional detection of @import and expression directives in CSS are now
    excluded.
    Certain MIME types are now always blacklisted.
    Frames, framesets, layers, ilayers, applets, and base tags are now
    always blacklisted.

--- HtmlSanitizer/HtmlSanitizer/HtmlSanitizer.cs
+++ HtmlSanitizer/HtmlSanitizer/HtmlSanitizer.cs
@@ -1,151 +1,258 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Xml;
-using HtmlAgilityPack;
+using System.Text.RegularExpressions; // here be dragons
 
 namespace Westwind.Web.Utilities
 {
-    public class HtmlSanitizer
+  public class HtmlSanitizer
+  {
+    /// <summary>
+    /// Prevents a default instance of the <see cref="HtmlSanitizer"/> class from being created.
+    /// </summary>
+    private HtmlSanitizer()
     {
-
-        public HashSet<string> BlackList = new HashSet<string>() 
-        {
-                { "script" },
-                { "iframe" },
-                { "form" },
-                { "object" },
-                { "embed" },
-                { "link" },                
-                { "head" },
-                { "meta" }
-        };
-
-        /// <summary>
-        /// Cleans up an HTML string and removes HTML tags in blacklist
-        /// </summary>
-        /// <param name="html"></param>
-        /// <returns></returns>
-        public static string SanitizeHtml(string html, params string[] blackList)
-        {
-            var sanitizer = new HtmlSanitizer();
-            if (blackList != null && blackList.Length > 0)
-            {
-                sanitizer.BlackList.Clear();
-                foreach (string item in blackList)
-                    sanitizer.BlackList.Add(item);
-            }
-            return sanitizer.Sanitize(html);
-        }
-
-        /// <summary>
-        /// Cleans up an HTML string by removing elements
-        /// on the blacklist and all elements that start
-        /// with onXXX .
-        /// </summary>
-        /// <param name="html"></param>
-        /// <returns></returns>
-        public string Sanitize(string html)
-        {
-            var doc = new HtmlDocument();
-
-            doc.LoadHtml(html);
-            SanitizeHtmlNode(doc.DocumentNode);
-
-            //return doc.DocumentNode.WriteTo();
-
-            string output = null;
-
-            // Use an XmlTextWriter to create self-closing tags
-            using (StringWriter sw = new StringWriter())
-            {
-                XmlWriter writer = new XmlTextWriter(sw);
-                doc.DocumentNode.WriteTo(writer);
-                output = sw.ToString();
-
-                // strip off XML doc header
-                if (!string.IsNullOrEmpty(output))
-                {
-                    int at = output.IndexOf("?>");
-                    output = output.Substring(at + 2);
-                }
-
-                writer.Close();
-            }
-            doc = null;
-
-            return output;
-        }
-
-        private void SanitizeHtmlNode(HtmlNode node)
-        {
-            if (node.NodeType == HtmlNodeType.Element)
-            {
-                // check for blacklist items and remove
-                if (BlackList.Contains(node.Name))
-                {
-                    node.Remove();
-                    return;
-                }
-
-                // remove CSS Expressions and embedded script links
-                if (node.Name == "style")
-                {
-                    var val = node.InnerHtml;
-                    if (string.IsNullOrEmpty(node.InnerText))
-                    {
-                        if (HasExpressionLinks(val) || HasScriptLinks(val) )
-                            node.ParentNode.RemoveChild(node);
-                    }
-                }
-
-                // remove script attributes
-                if (node.HasAttributes)
-                {
-                    for (int i = node.Attributes.Count - 1; i >= 0; i--)
-                    {
-                        HtmlAttribute currentAttribute = node.Attributes[i];
-
-                        var attr = currentAttribute.Name.ToLower();
-                        var val = currentAttribute.Value.ToLower();
-
-                        // remove event handlers
-                        if (attr.StartsWith("on"))
-                            node.Attributes.Remove(currentAttribute);
-
-                        // Remove CSS Expressions
-                        else if (attr == "style" &&
-                                 val != null &&
-                                 HasExpressionLinks(val) || HasScriptLinks(val))
-                            node.Attributes.Remove(currentAttribute);
-
-                        // remove script links from all attributes
-                        else if (
-                            //(attr == "href" || attr== "src" || attr == "dynsrc" || attr == "lowsrc") &&
-                                 val != null &&
-                                 HasScriptLinks(val) )                                 
-                            node.Attributes.Remove(currentAttribute);
-                    }
-                }
-            }
-
-            // Look through child nodes recursively
-            if (node.HasChildNodes)
-            {
-                for (int i = node.ChildNodes.Count - 1; i >= 0; i--)
-                {
-                    SanitizeHtmlNode(node.ChildNodes[i]);
-                }
-            }
-        }
-
-        private bool HasScriptLinks(string value)
-        {
-            return value.Contains("javascript:") || value.Contains("vbscript:");
-        }
-
-        private bool HasExpressionLinks(string value)
-        {
-            return value.Contains("expression");
-        }
     }
+
+    /// <summary>
+    /// A regex for detecting expressions within values for CSS properties.
+    /// </summary>
+    /// <remarks>
+    /// I wish this wasn't needed, but right now the CSS parsing library
+    /// we're using doesn't even recognize expressions as valid.
+    /// If only browsers worked the same way.
+    /// </remarks>
+    private static readonly Regex _cssExpressionRegex =
+      new Regex(@":\s*expression\s*\(", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+    /// <summary>
+    /// The set of HTML tags that are blacklisted.
+    /// </summary>
+    private static readonly HashSet<string> _blacklistedTags = new HashSet<string>(System.StringComparer.InvariantCultureIgnoreCase) 
+    {
+      // Head elements and other such nonsense
+      { "head" },
+      { "link" }, 
+      { "meta" },
+      { "base" },
+      { "form" },
+      { "script" },
+      // Embeddables
+      { "applet" },
+      { "object" },
+      { "embed" },
+      /// Frames
+      { "frameset" },
+      { "frame" },
+      { "iframe" },
+      // Layers
+      { "layer" },
+      { "ilayer" }
+    };
+
+    /// <summary>
+    /// The set of MIME types that are blacklisted.
+    /// </summary>
+    private static readonly HashSet<string> _blacklistedMimeTypes = new HashSet<string>(System.StringComparer.InvariantCultureIgnoreCase)
+    {
+      { "text/javascript" },
+      { "text/ecmascript" },
+      { "application/ecmascript" },
+      { "application/javascript" },
+      { "text/vbscript" },
+      { "javascript" }
+    };
+
+    /// <summary>
+    /// Cleans up an HTML string by removing elements on the blacklist and all elements that start
+    /// with onXXX.
+    /// </summary>
+    /// <param name="html">The HTML string to sanitize.</param>
+    /// <returns>The sanitized version of the HTML.</returns>
+    public static string SanitizeHtml(string html)
+    {
+      // Load in the HTML string into an HTML document
+      var doc = new HtmlAgilityPack.HtmlDocument();
+      doc.LoadHtml(html);
+
+      // Sanitize the root document node
+      SanitizeHtmlNode(doc.DocumentNode);
+
+      string output = null;
+
+      // Use an XmlTextWriter to create self-closing tags
+      using (StringWriter sw = new StringWriter())
+      {
+        XmlWriter writer = new XmlTextWriter(sw);
+        doc.DocumentNode.WriteTo(writer);
+        output = sw.ToString();
+
+        // strip off XML doc header
+        if (!string.IsNullOrEmpty(output))
+        {
+          int at = output.IndexOf("?>");
+          output = output.Substring(at + 2);
+        }
+
+        writer.Close();
+      }
+      doc = null;
+
+      return output;
+    }
+
+    private static void SanitizeHtmlNode(HtmlAgilityPack.HtmlNode node)
+    {
+      if (node.NodeType == HtmlAgilityPack.HtmlNodeType.Element)
+      {
+        // Remove elements if they're blacklisted
+        if (_blacklistedTags.Contains(node.Name))
+        {
+          node.Remove();
+          return;
+        }
+
+        // For style tags, don't allow remote-linked stylesheets or
+        // items with CSS expressions/import directives
+        if (node.Name == "style")
+        {
+          // Remove remote-linked stylesheets
+          if (string.IsNullOrWhiteSpace(node.InnerText))
+          {
+            node.Remove();
+            return;
+          }
+
+          // HACK: Look for CSS expressions, because
+          // ExCSS doesn't successfully parse them out.
+          if (_cssExpressionRegex.IsMatch(node.InnerText))
+          {
+            node.Remove();
+            return;
+          }
+
+          // Next, try to parse out the CSS using ExCSS.
+          // If it fails, we'll remove the node too.
+          try
+          {
+            var stylesheetParser = new ExCSS.Parser();
+            var stylesheet = stylesheetParser.Parse(node.InnerText);
+
+            // Make sure we have no import directives.
+            if (stylesheet.ImportDirectives.Count > 0)
+            {
+              node.Remove();
+              return;
+            }
+
+            // Note: this is disabled because ExCSS doesn't even recognize
+            // CSS expressions at the moment.
+            //// Make sure the value of no property declaration starts with
+            //// expression(
+            //foreach (ExCSS.StyleRule ruleset in stylesheet.Rulesets)
+            //{
+            //  foreach (ExCSS.Property propDecl in ruleset.Declarations)
+            //  {
+            //    if (propDecl.Term.ToString().Trim().ToLower().StartsWith("expression("))
+            //    {
+            //      node.Remove();
+            //      return;
+            //    }
+            //  }
+            //}
+          }
+          catch
+          {
+            // Some sort of parsing error, which means this style
+            // declaration is invalid.
+            node.Remove();
+            return;
+          }
+        }
+
+        // remove script attributes
+        if (node.HasAttributes)
+        {
+          for (int i = node.Attributes.Count - 1; i >= 0; i--)
+          {
+            HtmlAgilityPack.HtmlAttribute currentAttribute = node.Attributes[i];
+
+            // Lowercase the attribute and its value for ease of use.
+            var attr = currentAttribute.Name.ToLower();
+            var val = currentAttribute.Value.ToLower();
+
+            // Remove event handlers
+            if (attr.StartsWith("on"))
+            {
+              node.Attributes.Remove(currentAttribute);
+            }
+            // Remove stuff with a forbidden MIME type - note that this removes
+            // the entire node, not just the attribute.
+            else if (attr == "type" && val != null && IsBlacklistedMimeType(val))
+            {
+              node.Remove();
+              return;
+            }
+            // Remove CSS Expressions
+            else if (attr == "style" && val != null && (HasExpressionOrImportLinks(val) || HasScriptLinks(val)))
+            {
+              node.Attributes.Remove(currentAttribute);
+            }
+            // Remove script links from all attributes
+            else if (val != null && HasScriptLinks(val))
+            {
+              node.Attributes.Remove(currentAttribute);
+            }
+          }
+        }
+      }
+
+      // Look through child nodes recursively
+      if (node.HasChildNodes)
+      {
+        for (int i = node.ChildNodes.Count - 1; i >= 0; i--)
+        {
+          SanitizeHtmlNode(node.ChildNodes[i]);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Determines whether the specified attribute value contains script links.
+    /// </summary>
+    /// <param name="value">The attribute value. It is assumed that this value
+    /// has already been normalized as lowercase.</param>
+    /// <returns><c>true</c> if the specified attribute value contains script links;
+    /// otherwise, <c>false</c>.</returns>
+    private static bool HasScriptLinks(string value)
+    {
+      return value.Contains("javascript:") || value.Contains("vbscript:");
+    }
+
+    /// <summary>
+    /// Determines whether the specified attribute value contains 
+    /// a CSS &quot;expression&quot; or &quot;@import&quot; rule.
+    /// </summary>
+    /// <param name="value">The attribute value. It is assumed that this value
+    /// has already been normalized as lowercase.</param>
+    /// <returns><c>true</c> if the specified attribute value contains a CSS expression
+    /// or import; otherwise, <c>false</c>.</returns>
+    private static bool HasExpressionOrImportLinks(string value)
+    {
+      return _cssExpressionRegex.IsMatch(value) || value.Contains("@import");
+    }
+
+    /// <summary>
+    /// Determines whether the specified attribute value is a blacklisted MIME type.
+    /// </summary>
+    /// <param name="value">The attribute value. It is assumed that this value
+    /// has already been normalized as lowercase.</param>
+    /// <returns><c>true</c> if the specified attribute value corresponds with
+    /// a blacklisted MIME type; otherwise, <c>false</c>.</returns>
+    private static bool IsBlacklistedMimeType(string value)
+    {
+      return _blacklistedMimeTypes.Contains(value.Trim());
+    }
+
+  }
 }
